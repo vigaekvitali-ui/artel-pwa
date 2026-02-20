d20e8f3b0c904507a2d9ec60e50c4b537b9f6bb2
vigaekvitali-ui <vigaekvitali@gmail.com>
2026-02-21 00:32:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
 const CACHE_NAME = 'artel-v1';
 const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  'https://cdn.jsdelivr.net/npm/gun/gun.js'
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/offline.html' // Страница 404/Offline с разбитым стаканом
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
+    .catch(() => caches.match('/offline.html'))
   );
 });
