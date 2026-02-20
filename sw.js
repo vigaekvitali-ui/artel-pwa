f5c9b7a1359755750cae8d4ad8f73c09c0aed18b
vigaekvitali-ui <vigaekvitali@gmail.com>
2026-02-20 22:30:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
 const CACHE_NAME = 'artel-v1';
 const assets = [
-    '/',
-    '/index.html',
-    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  'https://cdn.jsdelivr.net/npm/gun/gun.js'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-    );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
